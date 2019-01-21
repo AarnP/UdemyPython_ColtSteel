884a003f3237be5d482c41b3d03e9824d8ca0a33
AaronPhan <aarn.phan@gmail.com>
2019-01-21 15:59:20 +1100

    chekc

--- or.py
+++ or.py
@@ -3,4 +3,4 @@ city = input("Where do you live? ")
 if city == "los angeles" or city == "san francisco":
     print("You live in California")
 else:
-    print("You live somewhere else")
+    print("You live somewhere else, mate")
