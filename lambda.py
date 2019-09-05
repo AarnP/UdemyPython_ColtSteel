232a473004991dd157a1606e64010b8ebcab53f1
Aaron Phan <aarn.phan@gmail.com>
2019-09-05 12:37:21 +1000

    Second part of Python Theory

--- lambda.py
+++ lambda.py
@@ -1,1 +1,23 @@
-lambda num: num * num
+# lambda num: num * num
+# names = [
+#     {'first': 'Rusty', 'last': 'Steel'},
+#     {'first': 'Aaron', 'last': 'Rock'},
+#     {'first': 'Dave', 'last': 'Van'}
+# ]
+#
+# first_name = list(map(lambda x: x['first'], names))
+# print(first_name)
+
+
+# def decrement_list(aList):
+#     return list(map(lambda x: x-1, aList))
+#
+#
+# print(decrement_list([2, 3, 4]))
+
+
+def remove_negatives(aList):
+    return list(filter(lambda num: num >= 0, aList))
+
+
+print(remove_negatives([1, -1, 2, -2, 3, -3]))
