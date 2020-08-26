c7df8dde6d9d1a701dcb6373732c5d1c67a67c7e
Aaron <aarn.phan@gmail.com>
2020-08-26 23:57:13 +1000

    update

--- dad.py
+++ dad.py
@@ -8,33 +8,6 @@
 # - Send request to search endpoint
 # - query string term whatever user TypeErro
 # - Use if condition
-<<<<<<< HEAD
-import random
-import requests
-import json
-
-tell_joke = input("Let me tell you a joke! Give me a topic: ")
-url = "https://icanhazdadjoke.com/search"
-response = requests.get(
-    url,
-    headers={"Accept": "text/plain"},
-    params={"term": tell_joke, "lim": 1}
-
-)
-# Get data into dictionary
-# data = response.json()
-# # Access data in list
-# data_in_list = data['results']
-# # Turn joke variable into a dictionary
-# joke = {}
-# for joke in data_in_list:
-#     print(joke['joke'])
-# print(type(joke))
-# def make_joke():
-#     if response
-data = response.text
-print(data)
-=======
 from random import choice
 import requests
 
@@ -56,4 +29,3 @@ elif num_joke == 1:
     print(results[0]['joke'])
 else:
     print(f"Sorry, couldn't find a joke with your term: {user_input}")
->>>>>>> 09d7747d4315c05c247dc796f95d13851849e9b1
